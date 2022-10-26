6ddc7ae62b6f6e2f233c60636389cba22cbc1705
Ashley <ashleyd3391@gmail.com>
2022-10-26 17:01:04 -0500

    Fixed issue where code returns a loss no matter the player and computer choice. Code was not recognizing the computer's choice and stated "undefined" which is why it returned the losses. Now it recognizes the computer choice and uses it in checkWinner function to return win, loss, or tie.

--- javascript.js
+++ javascript.js
@@ -1,37 +1,45 @@
 const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
-    const choice = options[Math.floor(Math.random() * 3)]
-    console.log(choice)
+    const choice = options[Math.floor(Math.random() * 3)];
+    console.log(choice);
+    return choice;
 }
 
 //Math.random can return random number between 0 and 1//
 //Math.floor is used to round up to nearest integer, since we have 3 options it is needed//
 
 function checkWinner(playerSelection, computerSelection){
-    if(
+    if(playerSelection == computerSelection){
+        return "Tie";
+    }
+    else if(
         (playerSelection == "rock" && computerSelection == "scissors") ||
         (playerSelection == "scissors" && computerSelection == "paper") ||
         (playerSelection == "paper" && computerSelection == "rock")
     ){
         return "Player";
     }
-    else{
-        return "Computer"
+    else {
+        return "Computer";
     }
+  
  }
+
+ checkWinner();
 
 function playRound(playerSelection, computerSelection){
     const result = checkWinner(playerSelection, computerSelection);
+
     if(result == "Tie"){
-        return "It's a tie";
+        return "It's a tie!"
     }
-    if(result == "Player"){
-        return "You win!"
+    else if(result == "Player"){
+        return `You win! ${playerSelection} beats ${computerSelection}`
     }
-    if(result == "Computer"){
-        return "You lose!"
-    }
+   else{
+    return `You lose! ${playerSelection} beats ${computerSelection}`
+   }
 }
 
 function getPlayerChoice(){
@@ -42,7 +50,7 @@ function getPlayerChoice(){
 function game(){
     console.log("Welcome!")
     for (let i = 0; i < 5; i++) {
-        const playerSelection = getPlayerChoice();
+        const playerSelection = "rock";
         const computerSelection = getComputerChoice();
         console.log(playRound(playerSelection, computerSelection));
     }
