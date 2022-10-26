36f643578b8d0dbb7a0fb336be961373cea72a6c
Ashley <ashleyd3391@gmail.com>
2022-10-26 17:32:04 -0500

    Added code that will accept user choice if capitalzed and only accepts validated terms for input. Added variables for player and computer score to keep track of wins.

--- javascript.js
+++ javascript.js
@@ -2,7 +2,6 @@ const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
     const choice = options[Math.floor(Math.random() * 3)];
-    console.log(choice);
     return choice;
 }
 
@@ -41,19 +40,47 @@ function playRound(playerSelection, computerSelection){
     return `You lose! ${playerSelection} beats ${computerSelection}`
    }
 }
-
+// validated input and array sused to ensure user is entering correct terms (rock, paper, or scissors) and will accept those terms even if capitalized. //
 function getPlayerChoice(){
-        const choice = prompt("Rock, paper, or scissors?");
-        
+        let validatedInput = false;
+        while(validatedInput == false){
+            const choice = prompt("Rock, paper, or scissors?");
+            if(choice == null){
+                continue;
+            }
+            const choiceInLower = choice.toLowerCase();
+            if(options.includes(choiceInLower)){
+                validatedInput = true;
+                return choiceInLower;
+            }
         }
+    }
 
 function game(){
+    let playerScore = 0;
+    let computerScore = 0;
     console.log("Welcome!")
     for (let i = 0; i < 5; i++) {
-        const playerSelection = "rock";
+        const playerSelection = getPlayerChoice();
         const computerSelection = getComputerChoice();
         console.log(playRound(playerSelection, computerSelection));
+        if(checkWinner(playerSelection,computerSelection) == "Player"){
+            playerScore++;
+        }
+        else if(checkWinner(playerSelection,computerSelection) == "Computer"){
+            computerScore++;
+        }
+    }
+    console.log("Game over!")
+    if(playerScore > computerScore){
+        console.log("Player was the winner")
+    }
+    else if(playerScore < computerScore){
+        console.log("Computer was the winner")
+    }
+    else{
+        console.log("We have a tie!")
     }
 }
-
+// game function will ask player for choice, return the computer choice, then determine who wins. It will loop for 5 rounds. //
 game()
